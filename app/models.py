a771dbec45dcec51f08934eb6d4ec998685ec5bc
Ding <zding5@illinois.edu>
2016-03-14 16:26:06 -0500

    more pages

--- app/models.py
+++ app/models.py
@@ -1,12 +1,12 @@
 from app import db
 from app import app
 
-class User(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(64), index=True, unique=True)
+# class User(db.Model):
+# 	id = db.Column(db.Integer, primary_key=True)
+# 	name = db.Column(db.String(64), index=True, unique=True)
 
-	def __repr__(self):
-		return '<User %r>' % (self.name)
+# 	def __repr__(self):
+# 		return '<User %r>' % (self.name)
 
 class Picture(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
@@ -16,3 +16,14 @@ class Picture(db.Model):
 
 	def __repr__(self):
 		return '<Pic #%r %r of link %r>' % (self.id, self.name, self.link)
+
+class Survey(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	# user = 
+	event = db.Column(db.String(400))
+	location = db.Column(db.String(400))
+	weather = db.Column(db.String(400))
+	style = db.Column(db.String(400))
+
+	def __repr__(self):
+		return '<This is survey #%r \n event: %r \n location: %r \n weather: %r \n style: %r \n>' % (self.id, self.event, self.location, self.weather, self.style)
--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
 from flask import render_template, redirect, url_for, jsonify
 from app import app, db
-from .models import Picture
+from .models import Picture, Survey
+from .forms import surveyForm
 import json
+import os
 
 @app.route('/')
 @app.route('/index')
@@ -9,23 +11,39 @@ def index():
 	pics = Picture.query.all()
 	return render_template('index.html', pics = pics)
 
-@app.route('/populate', methods=['POST'])
-def populate():
-	if Picture.query.filter_by(link="f8d2a81b4c09364384a4322406ad47e8ce7ba38b.jpg").first() is None:
-		u = Picture(name="Sheer jacket", link="f8d2a81b4c09364384a4322406ad47e8ce7ba38b.jpg")
-		db.session.add(u)
-		db.session.commit()
-	# return json.dumps({'name':'Sheer jacket', 'id':'2', 'link':'f8d2a81b4c09364384a4322406ad47e8ce7ba38b.jpg'})
-	return_data = {'name':'Sheer jacket', 'id':'2', 'link':'f8d2a81b4c09364384a4322406ad47e8ce7ba38b.jpg'}
-	return jsonify(return_data)
 
 
-@app.route('/deleter', methods=['DELETE'])
-def delete():
-	u = Picture.query.filter_by(link="f8d2a81b4c09364384a4322406ad47e8ce7ba38b.jpg").first()
-	delid = u.id
-	if u is not None:
-		db.session.delete(u)
+@app.route('/page3', methods=['GET','POST'])
+def survey():
+	form = surveyForm()
+	if form.validate_on_submit():
+		survey_result = Survey(event=form.event.data,
+								location=form.location.data,
+								weather=form.weather.data,
+								style=form.style.data)
+		db.session.add(survey_result)
 		db.session.commit()
-	return jsonify({'delid':delid})
+		os.system('echo "Hello!" ')
+		return redirect(url_for('index'))
+	return render_template('page3.html', form=form)
+
+
+# @app.route('/populate', methods=['POST'])
+# def populate():
+# 	if Picture.query.filter_by(link="f8d2a81b4c09364384a4322406ad47e8ce7ba38b.jpg").first() is None:
+# 		u = Picture(name="Sheer jacket", link="f8d2a81b4c09364384a4322406ad47e8ce7ba38b.jpg")
+# 		db.session.add(u)
+# 		db.session.commit()
+# 	return_data = {'name':'Sheer jacket', 'id':'2', 'link':'f8d2a81b4c09364384a4322406ad47e8ce7ba38b.jpg'}
+# 	return jsonify(return_data)
+
+
+# @app.route('/deleter', methods=['DELETE'])
+# def delete():
+# 	u = Picture.query.filter_by(link="f8d2a81b4c09364384a4322406ad47e8ce7ba38b.jpg").first()
+# 	delid = u.id
+# 	if u is not None:
+# 		db.session.delete(u)
+# 		db.session.commit()
+# 	return jsonify({'delid':delid})
 
