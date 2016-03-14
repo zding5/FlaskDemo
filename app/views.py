8015b40f1d4fba4ca1bcb20a32c05262be5220b6
Ding <zding5@illinois.edu>
2016-03-14 17:36:34 -0500

    fixed

--- app/views.py
+++ app/views.py
@@ -24,24 +24,25 @@ def survey():
 		db.session.add(survey_result)
 		db.session.commit()
 		# os.system('echo "Hello!" ')
-		tempstring = "boot jacket leather studded t-shirt black commando distressed fishnet alexander-mcqueen sweatshirt-hoodies"
-		with open("output.txt", "w+") as f:
-			f.write(tempstring)
+		# tempstring = "boot jacket leather studded t-shirt black commando distressed fishnet alexander-mcqueen sweatshirt-hoodies"
+		# with open("output.txt", "w+") as f:
+			# f.write(tempstring)
 
-		command1 = "/home/dddg/mallet-2.0.8RC3/bin/mallet import-file --input output.txt --output output.sequences --keep-sequence --token-regex '[\p{L}\p{P}\p{N}]*\p{L}' --use-pipe-from app/mallet/training.sequences"
+		# command1 = "/home/dddg/mallet-2.0.8RC3/bin/mallet import-file --input output.txt --output output.sequences --keep-sequence --token-regex '[\p{L}\p{P}\p{N}]*\p{L}' --use-pipe-from app/mallet/training.sequences"
 		# os.system(command1)
-		# return redirect(url_for('index'))
+		# command2 = "/home/dddg/mallet-2.0.8RC3/bin/mallet infer-topics --input output.sequences --inferencer app/mallet/inferencer.output.0 --output-doc-topics doctops"
+		return redirect(url_for('index'))
 	return render_template('page3.html', form=form)
 
 
-# @app.route('/populate', methods=['POST'])
-# def populate():
-# 	if Picture.query.filter_by(link="f8d2a81b4c09364384a4322406ad47e8ce7ba38b.jpg").first() is None:
-# 		u = Picture(name="Sheer jacket", link="f8d2a81b4c09364384a4322406ad47e8ce7ba38b.jpg")
-# 		db.session.add(u)
-# 		db.session.commit()
-# 	return_data = {'name':'Sheer jacket', 'id':'2', 'link':'f8d2a81b4c09364384a4322406ad47e8ce7ba38b.jpg'}
-# 	return jsonify(return_data)
+@app.route('/populate', methods=['POST'])
+def populate():
+	if Picture.query.filter_by(link="f8d2a81b4c09364384a4322406ad47e8ce7ba38b.jpg").first() is None:
+		u = Picture(name="Sheer jacket", link="f8d2a81b4c09364384a4322406ad47e8ce7ba38b.jpg")
+		db.session.add(u)
+		db.session.commit()
+	return_data = {'name':'Sheer jacket', 'id':'2', 'link':'f8d2a81b4c09364384a4322406ad47e8ce7ba38b.jpg'}
+	return jsonify(return_data)
 
 
 # @app.route('/deleter', methods=['DELETE'])
