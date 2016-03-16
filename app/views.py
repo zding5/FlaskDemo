c9cbe2ade7e79fad3d61623af85533918a741df8
Ding <zding5@illinois.edu>
2016-03-16 00:16:45 -0500

    views

--- app/views.py
+++ app/views.py
@@ -33,23 +33,23 @@ def survey():
 		# command2 = "/home/dddg/mallet-2.0.8RC3/bin/mallet infer-topics --input output.sequences --inferencer app/mallet/inferencer.output.0 --output-doc-topics doctops"
 		# command2 = "mallet-2.0.8RC3/bin/mallet infer-topics --input app/mallet/output.sequences --inferencer app/mallet/inferencer.output.0 --output-doc-topics doctops"
 
-		# command1 = "mallet-2.0.8RC3/bin/mallet import-file --input app/mallet/words.txt --output app/mallet/output.sequences --keep-sequence --token-regex '[\p{L}\p{P}\p{N}]*\p{L}' --use-pipe-from app/mallet/training.sequences"
-		command1 = "/home/dddg/mallet-2.0.8RC3/bin/mallet import-file --input app/mallet/words.txt --output app/mallet/output.sequences --keep-sequence --token-regex '[\p{L}\p{P}\p{N}]*\p{L}' --use-pipe-from app/mallet/training.sequences"
-		# command2 = "mallet-2.0.8RC3/bin/mallet infer-topics --input app/mallet/output.sequences --inferencer app/mallet/inferencer.output.0 --output-doc-topics app/mallet/doctops"
-		command2 = "/home/dddg/mallet-2.0.8RC3/bin/mallet infer-topics --input app/mallet/output.sequences --inferencer app/mallet/inferencer.output.0 --output-doc-topics app/mallet/doctops"
+		command1 = "mallet-2.0.8RC3/bin/mallet import-file --input app/mallet/words.txt --output app/mallet/output.sequences --keep-sequence --token-regex '[\p{L}\p{P}\p{N}]*\p{L}' --use-pipe-from app/mallet/training.sequences"
+		# command1 = "/home/dddg/mallet-2.0.8RC3/bin/mallet import-file --input app/mallet/words.txt --output app/mallet/output.sequences --keep-sequence --token-regex '[\p{L}\p{P}\p{N}]*\p{L}' --use-pipe-from app/mallet/training.sequences"
+		command2 = "mallet-2.0.8RC3/bin/mallet infer-topics --input app/mallet/output.sequences --inferencer app/mallet/inferencer.output.0 --output-doc-topics app/mallet/doctops"
+		# command2 = "/home/dddg/mallet-2.0.8RC3/bin/mallet infer-topics --input app/mallet/output.sequences --inferencer app/mallet/inferencer.output.0 --output-doc-topics app/mallet/doctops"
 
-		command3 = "/home/dddg/mallet-2.0.8RC3/bin/mallet import-file --input app/mallet/styles.txt --output app/mallet/style_output.sequences --keep-sequence --token-regex '[\p{L}\p{P}\p{N}]*\p{L}' --use-pipe-from app/mallet/descriptor.sequences"
-		command4 = "/home/dddg/mallet-2.0.8RC3/bin/mallet infer-topics --input app/mallet/style_output.sequences --inferencer app/mallet/inferencer.output.1 --output-doc-topics app/mallet/style_doctops"
+		# command3 = "/home/dddg/mallet-2.0.8RC3/bin/mallet import-file --input app/mallet/styles.txt --output app/mallet/style_output.sequences --keep-sequence --token-regex '[\p{L}\p{P}\p{N}]*\p{L}' --use-pipe-from app/mallet/descriptor.sequences"
+		# command4 = "/home/dddg/mallet-2.0.8RC3/bin/mallet infer-topics --input app/mallet/style_output.sequences --inferencer app/mallet/inferencer.output.1 --output-doc-topics app/mallet/style_doctops"
 
 		os.system(command1)
 		print("done")
 		os.system(command2)
 		print("done2")
 
-		os.system(command3)
-		print("done3")
-		os.system(command4)
-		print("done4")
+		# os.system(command3)
+		# print("done3")
+		# os.system(command4)
+		# print("done4")
 
 		return redirect(url_for('index'))
 	return render_template('page3.html', form=form)
