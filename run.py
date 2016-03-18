c31355c98b668e33a30cbcc542afb2ea1cc2769f
Ding <zding5@illinois.edu>
2016-03-18 01:33:55 -0500

    run.py

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!flask/bin/python
 from app import app
-# app.run(debug=True)
-# app.run(host="0.0.0.0", port=2000)
 # app.run(host="0.0.0.0", port=80)
 if __name__ == "__main__":
+	# app.run(debug=True)
 	app.debug = True
-	app.run()
+	app.run(host="0.0.0.0", port=2000)
+# 	app.run()
