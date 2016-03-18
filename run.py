dd83dd248f8433fcbc4406a14269a3d27b532e36
Ding <zding5@illinois.edu>
2016-03-17 23:58:11 -0500

    a

--- run.py
+++ run.py
@@ -2,4 +2,7 @@
 from app import app
 # app.run(debug=True)
 # app.run(host="0.0.0.0", port=2000)
-app.run(host="0.0.0.0", port=80)
+# app.run(host="0.0.0.0", port=80)
+if __name__ == "__main__":
+	app.debug = True
+	app.run()
