614f822b85e7a6ddc9780041d8f8c0ef5c54cd79
Ding <zding5@illinois.edu>
2016-04-09 23:26:25 -0500

    new

--- app/forms.py
+++ app/forms.py
@@ -4,15 +4,14 @@ from wtforms.validators import DataRequired, Length
 
 class surveyForm(Form):
 	# for page 3
-	event = StringField('event', validators=[DataRequired()])
-	location = StringField('location', validators=[DataRequired()])
-	weather = StringField('weather', validators=[DataRequired()])
-	style = StringField('style', validators=[DataRequired()])
+	event = StringField('event')
+	location = StringField('location')
+	weather = StringField('weather')
+	style = StringField('style')
 
 # class outfitcollection(Form):
 class outfitForm(Form):
 	# page 1
-	# outfit_one = StringField('outfit_one', validators=[DataRequired()])
-	outfit_one = TextAreaField('outfit_one', validators=[DataRequired()])
-	outfit_two = StringField('outfit_two', validators=[DataRequired()])
-	outfit_three = StringField('outfit_three', validators=[DataRequired()])
+	outfit_one = TextAreaField('outfit_one')
+	outfit_two = TextAreaField('outfit_two')
+	outfit_three = TextAreaField('outfit_three')
--- app/views.py
+++ app/views.py
@@ -16,20 +16,23 @@ LOCAL_MALLET_APP = "mallet-2.0.8RC3/bin/mallet"
 topics = None
 items = None
 phiMatrices = None
+toppage1 = None
+toppage3 = None
 topItemsByTopic = None
 
-# url2lab_file = "data/labelLookupNew.p"
-# url2jpg_file = "data/jpg2Url.p"
+url2lab_file = "data/labelLookupNew.p"
+url2jpg_file = "data/jpg2Url.p"
 
-# url2lab = pickle.load(open(url2lab_file, "rb"))
-# url2jpg = pickle.load(open(url2jpg_file, "rb"))
-# jpg2lab = dict()
-# for k in url2lab.keys():
-    # if k in url2jpg.keys():
-        # jpgname = url2jpg[k]
-        # jpg2lab[jpgname] = url2lab[k]
+url2lab = pickle.load(open(url2lab_file, "rb"))
+url2jpg = pickle.load(open(url2jpg_file, "rb"))
+jpg2lab = dict()
+for k in url2lab.keys():
+    if k in url2jpg.keys():
+        jpgname = url2jpg[k]
+        jpg2lab[jpgname] = url2lab[k]
 
-# km = dict()
+km = dict()
+numk = 5
 
 @app.route('/', methods=['GET','POST'])
 @app.route('/index', methods=['GET','POST'])
@@ -46,98 +49,73 @@ def index():
 
 @app.route('/page1', methods=['GET','POST'])
 def survey_page1():
-	# form = outfitcollection()
-	form = outfitForm()
 
+	form = outfitForm()
+	if form.validate_on_submit():
+		outfits = Useroutfits(outfit_one=form.outfit_one.data, outfit_two=form.outfit_two.data, outfit_three=form.outfit_three.data)
+		db.session.add(outfits)
+		db.session.commit()
+	
+		comp_str = form.outfit_one.data+" "+form.outfit_two.data+" "+form.outfit_three.data
+		file_saver_local("concrete", comp_str)
+		mallet_runner_local("concrete")
+		global topics
+		global phiMatrices
+		global toppage1
+		toppage1 = getItemsByStyle(topics,phiMatrices,"concrete")
+		print(toppage1)
+		return redirect(url_for('survey_page2'))
 	return render_template('page1.html', form=form)
 
-@app.route('/page11', methods=['GET','POST'])
-def handle_survey_page1():
-	
-	form = outfitForm(request.form)
-	# if form.validate_on_submit():
-		# outfits = useroutfits(outfit_one=form.outfit_one.data, outfit_two=form.outfit_two.data, outfit_three=form.outfit_three.data)
-	outfits = Useroutfits(outfit_one=form.outfit_one.data, outfit_two=form.outfit_two.data, outfit_three=form.outfit_three.data)
-	db.session.add(outfits)
-	db.session.commit()
-
-	comp_str = form.outfit_one.data+" "+form.outfit_two.data+" "+form.outfit_three.data
-
-	file_saver_local("concrete", comp_str)
-	mallet_runner_local("concrete")
-	# file_saver_AWS("concrete", comp_str)
-	# mallet_runner_AWS("concrete")
-
-	global topics
-	global phiMatrices
-
-	topItemsByStyleWord = getItemsByStyle(topics,phiMatrices,"concrete")
-	print(topItemsByStyleWord)
-	return jsonify(topItemsByStyleWord)
 
 @app.route('/page2', methods=['GET', 'POST'])
 def survey_page2():
-	pass
-
-@app.route('/page21', methods=['GET', 'POST'])
-def handle_survey_page2():
-	pass
+	return render_template('page2.html', results=toppage1)
 
 @app.route('/page3', methods=['GET','POST'])
 def survey_page3():
-	form = surveyForm()
-
-	return render_template('page3.html', form=form)
-
-@app.route('/page31', methods=['POST','GET'])
-def handle_survey_page3():
 	print(request.form)
 	form = surveyForm(request.form)
-	print(form.event.data)
-	survey_result = Survey(event=form.event.data,
+	if form.validate_on_submit():
+		print(form.event.data)
+		survey_result = Survey(event=form.event.data,
 							location=form.location.data,
 							weather=form.weather.data,
 							style=form.style.data)
-	db.session.add(survey_result)
-	db.session.commit()
+		db.session.add(survey_result)
+		db.session.commit()
 
-	comp_str = form.event.data+" "+form.location.data+" "+form.weather.data+" "+form.style.data
-	file_saver_local("abstract", comp_str)
-	mallet_runner_local("abstract")
+		comp_str = form.event.data+" "+form.location.data+" "+form.weather.data+" "+form.style.data
+		file_saver_local("abstract", comp_str)
+		mallet_runner_local("abstract")
+		global topics
+		global phiMatrices
+		global toppage3
+		toppage3 = getItemsByStyle(topics,phiMatrices,"abstract")
+		print(toppage3)
 
-	global topics
-	global phiMatrices
+		result_words = toppage3[list(toppage3.keys())[0]]
+		top100, allwords = ComputeMatch(jpg2lab, result_words)
+		bin_dict = gen_binary_lists(allwords, top100)
+		global km
+		km = kmeans_clustering(allwords, bin_dict, k=numk)
 
-	topItemsByStyleWord = getItemsByStyle(topics,phiMatrices,"abstract")
-	print(topItemsByStyleWord)
-	return jsonify(topItemsByStyleWord)
+		return redirect(url_for('survey_page4'))
 	
-	# result_words = topItemsByStyleWord[list(topItemsByStyleWord.keys())[0]]
-	# top100, allwords = ComputeMatch(jpg2lab, result_words)
-	# bin_dict = gen_binary_lists(allwords, top100)
-	# global km
-	# km = kmeans_clustering(allwords, bin_dict)
-	# for label in km.keys():
-	# 	print("cluster"+str(label))
-	# 	for key in km[label]:
-	# 		print(key, jpg2lab[key])
-	# 	print("\n\n\n")
-	# return "done"
+	return render_template('page3.html', form=form)
 
 
 @app.route('/page4', methods=['GET','POST'])
 def survey_page4():
-	global km
+	return	render_template('page4.html', results=km, numk=numk)
+	# global km
 	# km = kmeans_clustering(allwords, bin_dict)
-	for label in km.keys():
-		print("cluster"+str(label))
-		for key in km[label]:
-			print(key, jpg2lab[key])
-		print("\n\n\n")
+	# for label in km.keys():
+		# print("cluster"+str(label))
+		# for key in km[label]:
+			# print(key, jpg2lab[key])
+		# print("\n\n\n")
 
-@app.route('/page41', methods=['GET','POST'])
-def handle_survey_page4():
-	pass
 
 @app.route('/thankyou', methods=['GET','POST'])
 def thankyou():
@@ -171,37 +149,6 @@ def mallet_runner_local(AorC):
 		os.system(command4)
 		print("done4")
 	return
-
-# def dataPreprocessing(labels,keyfile,wordweightfile):
-# 	topics = {}
-# 	with open(keyfile) as f:
-# 		for lines in f:
-# 			if len(lines.split('\t')) == 2:
-# 				data = lines.split('\t')
-# 				topics[data[0]] = {}
-# 				topics[data[0]]['weight'] = float(data[1].strip())
-# 	with open(wordweightfile) as f:
-# 		for lines in f:
-# 			line = lines.split('\t')
-# 			if 'data' not in topics[line[0]]:
-# 				topics[line[0]]['data']={}
-# 				for l in labels:
-# 					topics[line[0]]['data'][l]={}
-# 			for l in labels:
-# 				if line[1] in labels[l]:
-# 					topics[line[0]]['data'][l][line[1]] = float(line[2].strip())
-# 	for t in topics:
-# 		topics[t]['totalweight']=0
-# 		for l in topics[t]['data']:
-# 			for elt in topics[t]['data'][l]:
-# 				topics[t]['totalweight']+= topics[t]['data'][l][elt]
-# 	items = {}
-# 	for t in topics:
-# 		for l in topics[t]['data']:
-# 			if l not in items:
-# 				items[l]=set()
-# 			items[l] = items[l] | set(topics[t]['data'][l].keys())
-# 	return (topics,items)
 
 def dataPreprocessing(labels,keyfile,wordweightfile):
 	topics = {}
@@ -267,39 +214,6 @@ def getTopics():
 			topItemsByTopic[t][l]=[i for i in reversed(sortres[-25:])]
 	return (topics, items, phiMatrices, topItemsByTopic)
 
-# def getTopics():
-# 	phiMatrices = {}
-# 	labels = {}
-# 	labels['concrete'] = pickle.load(open("app/mallet/concrete.p","rb"))
-# 	labels['abstract'] = pickle.load(open("app/mallet/abstract.p","rb"))
-# 	wordweightfile = "app/mallet/topicWordWeight.output"
-# 	keyfile = "app/mallet/icmsdkeys.txt"
-# 	# labels['concrete'] = pickle.load(open(AWS_MALLET_FILES+"concrete.p","rb"))
-# 	# labels['abstract'] = pickle.load(open(AWS_MALLET_FILES+"abstract.p","rb"))
-# 	# wordweightfile = AWS_MALLET_FILES+"topicWordWeight.output"
-# 	# keyfile = AWS_MALLET_FILES+"icmsdkeys.txt"
-
-# 	(topics,items)= dataPreprocessing(labels, keyfile,wordweightfile)
-# 	# mp = computeMarginalProb(items,topics)
-# 	numTopics = 150
-# 	for jj in topics:
-# 	# for j in range(numTopics):
-# 		# jj = str(j)
-# 		for l in topics['0']['data']:
-# 			vals = np.array([i/float(topics[jj]['totalweight'][l]) for i in topics[jj]['data'][l].values()])
-# 			try:
-# 				phiMatrices[l]=np.vstack((phiMatrices[l],vals))
-# 			except:
-# 				phiMatrices[l]=vals
-# 	topItemsByTopic = {}
-# 	for t in topics:
-# 		if t not in topItemsByTopic:
-# 			topItemsByTopic[t]={}
-# 		for l in topics['0']['data']:
-# 			res = topics[t]['data'][l]
-# 			sortres = sorted(res.items(),key=operator.itemgetter(1))
-# 			topItemsByTopic[t][l]=sortres[-25:]
-# 	return (topics, items, phiMatrices, topItemsByTopic)
 
 def getItemsByStyle(topics,phiMatrices,AorC):
 	inferredTheta = {}
@@ -334,43 +248,9 @@ def getItemsByStyle(topics,phiMatrices,AorC):
 		inds = sorted(range(len(rawres)), key=lambda k: rawres[k])
 		tempres = [itemnames[i] for i in reversed(inds[-25:])]
 		topItemsByStyleWord[sty]=tempres
+		print(inferredTheta)
 	return topItemsByStyleWord
 
-# def getItemsByStyle(topics,phiMatrices,AorC):
-# 	inferredTheta = {}
-# 	if AorC == "concrete":
-# 		thetafile = "app/mallet/c2adoctops"
-# 		# thetafile = AWS_MALLET_FILES+"c2adoctops"
-# 	else:
-# 		thetafile = "app/mallet/a2cdoctops"
-# 		# thetafile = AWS_MALLET_FILES+"a2cdoctops"
-# 	with open(thetafile,"r") as f:
-# 		for lines in f:
-# 			data = lines.split('\t')
-# 			try:
-# 				float(data[0])
-# 				if data[1] not in inferredTheta:
-# 	                # print data
-# 					# inferredTheta[data[1]] = [float(i.strip()) for i in data[2:]]
-# 					inferredTheta[data[1]] = [1.0] + [float(0)] * len(data[3:])
-
-# 					print (inferredTheta[data[1]])
-# 			except:
-# 				count = 0
-# 	topItemsByStyleWord = {}
-# 	for sty in inferredTheta:
-# 		if AorC == "concrete":
-# 			l = "abstract"
-# 		else:
-# 			l = "concrete"
-# 		topItemsByStyleWord[sty]={}
-# 		#            for lang in phiMatrices:
-# 		rawres = np.dot(inferredTheta[sty],phiMatrices[l])
-# 		itemnames = list(topics['0']['data'][l].keys())
-# 		inds = sorted(range(len(rawres)), key=lambda k: rawres[k])
-# 		tempres = [itemnames[i] for i in reversed(inds[-25:])]
-# 		topItemsByStyleWord[sty]=tempres
-# 	return topItemsByStyleWord
 
 def fixed_heap_insert(h, size, input):
 	if len(h) < size:
